46d4ab4f0bd9a7e2ef2478a6ff6733aa66487b8c
Julia Veronica <julia.veronicagdu@gmail.com>
2020-06-07 22:16:32 +0700

    challange3 done

--- challange3.js
+++ challange3.js
@@ -3,5 +3,13 @@
 function whosPaying(names) {
   // Don't change the code above.
   // write your code here.
+  // (comment) how many names on list.
+  var numberOfPeople = names.length;
+  var randomPersonPosition = Math.floor(Math.random() * numberOfPeople);
+  var randomPerson = names[randomPersonPosition];
+  return randomPerson + " is going to buy lunch today.";
   // Don't change the code above.
 }
+// run: whosPaying("A", "B", "C", "D", "E", "F") when enter: "A is going to buy lunch today."
+// example input: ["A", "B", "C", "D", "E", "F"].
+// example output: "A is going to buy lunch today."
